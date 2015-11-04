95d36311e408c8822ba4c63fb88bb8b73bb24099
Lukasz Swiderski <lukasz@swiderski.xyz>
2015-11-04 21:09:04 +0000

    update db to rankings and passwords

---
+++ WebProject/db_query_update_1.sql
@@ -0,0 +1,27 @@
+USE webprojectDB
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.[User] ADD
+	Country nchar(50) NULL
+GO
+ALTER TABLE dbo.[User]
+	DROP COLUMN Phone
+GO
+ALTER TABLE dbo.Place ADD
+	Ranking int NULL
+GO
+CREATE TABLE [Ranking]
+(
+    [PlaceId] INT NOT NULL,
+    [UserId] INT NOT NULL,
+    [Score] INT NOT NULL,
+) 
+ALTER TABLE [Ranking]
+    ADD CONSTRAINT FKRUserId FOREIGN KEY ([UserId]) REFERENCES [User] ([UserId])
+ALTER TABLE [Ranking]
+    ADD CONSTRAINT FKRPlaceId FOREIGN KEY ([PlaceId]) REFERENCES [Place] ([PlaceId])  
+GO
+ALTER TABLE dbo.[User] ADD
+	Password nvarchar(128) NOT NULL DEFAULT 'qwe'
+GO
+COMMIT
