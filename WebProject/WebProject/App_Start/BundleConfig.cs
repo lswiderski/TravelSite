b7b1c1ded98fcd0d96f05bb7754afbf0b9ec4e98
Lukasz Swiderski <lukasz@swiderski.xyz>
2015-11-24 22:29:26 +0000

    lightbox2 plugin

--- WebProject/WebProject/App_Start/BundleConfig.cs
+++ WebProject/WebProject/App_Start/BundleConfig.cs
@@ -8,8 +8,11 @@ namespace WebProject
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lightbox").Include(
+                       "~/Scripts/lightbox-2.6.js"));
+
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,7 +28,8 @@ namespace WebProject
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/lightbox.css"));
         }
     }
 }
